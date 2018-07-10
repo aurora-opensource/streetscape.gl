ff3d6cae5c6922aa7f0cee0bf477df9cb8d773b3
Xiaoji Chen <xiaoji@uber.com>
2018-07-10 11:44:35 -0700

    Fix lint & yarn lockfile

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -10,8 +10,7 @@ prettier-check "{modules,examples,test}/**/*.js" || \
 eslint src
 
 # check if yarn.lock contains private registry information
-# comment out for now to unblock
-# [ -n "`grep unpm.u yarn.lock`" ] && \
-#   echo 'Please rebuild yarn file using public npmrc' && \
-#   exit 1 || \
-#   echo 'Lockfile valid.'
+[ -n "`grep unpm.u yarn.lock`" ] && \
+  echo 'Please rebuild yarn file using public npmrc' && \
+  exit 1 || \
+  echo 'Lockfile valid.'
