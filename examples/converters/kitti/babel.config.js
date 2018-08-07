08f2f9f77f81e38b192cb2096efbeb2cc85b6c75
Tim Wojtaszek <twojtasz@uber.com>
2018-08-07 12:34:33 -0700

    Remove unnecessary babel bits from kitti converter

--- examples/converters/kitti/babel.config.js
+++ examples/converters/kitti/babel.config.js
@@ -23,24 +23,6 @@ module.exports = function babelConfig(api) {
 
   return {
     presets: ['@babel/preset-env'],
-    plugins: [
-      ['@babel/plugin-proposal-decorators', {legacy: true}],
-      '@babel/plugin-proposal-function-sent',
-      '@babel/plugin-proposal-function-bind',
-      '@babel/plugin-proposal-do-expressions',
-      '@babel/plugin-proposal-export-namespace-from',
-      '@babel/plugin-proposal-export-default-from',
-      '@babel/plugin-proposal-logical-assignment-operators',
-      '@babel/plugin-proposal-nullish-coalescing-operator',
-      '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-numeric-separator',
-      '@babel/plugin-proposal-throw-expressions',
-
-      // Stage 3
-      '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-syntax-import-meta',
-      ['@babel/plugin-proposal-class-properties', {loose: false}],
-      '@babel/plugin-proposal-json-strings'
-    ]
+    plugins: [['@babel/plugin-proposal-class-properties', {loose: false}]]
   };
 };
