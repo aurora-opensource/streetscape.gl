38561a91e1f099db8525b6a4fe9e3d46fa239745
Xiaoji Chen <xiaoji@uber.com>
2018-08-22 16:13:06 -0700

    wip

---
+++ modules/core/src/components/declarative-ui/xviz-container.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class XvizContainer extends PureComponent<Props> {
+
+  static propTypes = {
+    layout: PropTypes.string
+  };
+
+  static defaultProps = {
+    layout: 'vertical'
+  };
+
+  render() {
+    const { layout } = this.props;
+
+    const layoutStyle = {
+      display: 'flex',
+      width: '100%'
+    };
+    const childStyle = {};
+
+    switch (layout) {
+    case 'vertical':
+      layoutStyle.flexDirection = 'column';
+      childStyle.flex = '0 0 auto';
+      break;
+
+    case 'horizontal':
+      layoutStyle.flexDirection = 'row';
+      childStyle.flex = '1 1 auto';
+      break;
+
+    default:
+      // Unknown layout type
+      return null;
+    };
+
+    return (
+      <div className="xviz-container" style={layoutStyle}>
+        {React.Children.map(this.props.children, child => (
+          <div style={childStyle}>
+            {child}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
---
+++ modules/core/src/components/declarative-ui/xviz-panel.js
@@ -0,0 +1,51 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import XvizContainer from './xviz-container';
+import XvizMetricComponent from './xviz-metric-component';
+
+// xviz type to component map
+const DEFAULT_COMPONENTS = {
+  container: XvizContainer,
+  // table
+  // tree_table
+  metric: XvizMetricComponent
+  // plot
+  // video
+};
+
+export default class XvizPanel extends PureComponent {
+
+  static propTypes = {
+    components: PropTypes.object
+  };
+
+  static defaultProps = {
+    components: DEFAULT_COMPONENTS
+  };
+
+  _renderItem = (item, components) => {
+    const { components } = this.props;
+    const XvizComponent = components[item.type];
+
+    if (!XvizComponent) {
+      return null;
+    }
+
+    return (
+      <XvizComponent key={item.id} {...item}>
+        {item.children && item.children.map(this._renderItem)}
+      </XvizComponent>
+    );
+  }
+
+  render() {
+    const { data, style } = this.props;
+
+    return (
+      <div className="xviz-panel" style={style} >
+        {data.children && data.children.map(this._renderItem)}
+      </div>
+    );
+  }
+}
