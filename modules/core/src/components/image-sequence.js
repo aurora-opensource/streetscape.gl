0e48a20a7a108fbb02ff91a10c71b0aa608daeb0
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2018-09-14 14:27:33 -0700

    Fix VideoPanel component (#51)

--- modules/core/src/components/image-sequence.js
+++ modules/core/src/components/image-sequence.js
@@ -1,18 +1,22 @@
 // @flow
+import {getXvizSettings} from '@xviz/parser';
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
-// The time window within which frames can be attributed to the current timestamp
-const TOLERANCE = 0.1;
 // Buffer size in seconds around the given timestamp
 // This is used to pre-load frames into the DOM tree so that the video plays smoothly
-const VIDEO_FRAME_BUFFER = 2;
+const VIDEO_FRAME_BUFFER = 10;
+
+const CONTAINER_STYLE = {
+  position: 'relative',
+  lineHeight: 0
+};
 
 /* Component that renders image sequence as video */
 export default class ImageSequence extends PureComponent {
   static propTypes = {
-    width: PropTypes.number.isRequired,
-    height: PropTypes.number.isRequired,
+    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
     // Array of frames to render, in shape of {timestamp, imageUrl}
     src: PropTypes.array, // eslint-disable-line
@@ -27,10 +31,12 @@ export default class ImageSequence extends PureComponent {
   };
 
   static defaultProps = {
-    brightness: 1.0,
-    contrast: 1.0,
-    saturate: 1.0,
-    invert: 0.0,
+    width: '100%',
+    height: 'auto',
+    // brightness: 1.0,
+    // contrast: 1.0,
+    // saturate: 1.0,
+    // invert: 0.0,
     src: []
   };
 
@@ -50,24 +56,28 @@ export default class ImageSequence extends PureComponent {
   _getCurrentFrames(props) {
     const {currentTime, src} = props;
 
-    const minBufferTime = currentTime - VIDEO_FRAME_BUFFER;
-    const maxBufferTime = currentTime + VIDEO_FRAME_BUFFER;
-
-    const buffer = src.filter(
-      frame => frame.timestamp >= minBufferTime && frame.timestamp <= maxBufferTime
-    );
-
-    let bestDelta = TOLERANCE;
+    let currentFrame = null;
+    let currentFrameIndex = -1;
+    let bestDelta = getXvizSettings().TIME_WINDOW;
 
     // Find the frame closest to the current timestamp
-    const currentFrame = buffer.reduce((bestMatch, frame) => {
-      const delta = Math.abs(frame.timestamp - currentTime);
-      if (delta < bestDelta) {
+    src.forEach((frame, i) => {
+      const delta = currentTime - frame.timestamp;
+      if (delta >= 0 && delta < bestDelta) {
         bestDelta = delta;
-        return frame;
+        currentFrame = frame;
+        currentFrameIndex = i;
       }
-      return bestMatch;
-    }, null);
+    });
+
+    const buffer =
+      currentFrameIndex >= 0
+        ? src.slice(
+            Math.max(0, currentFrameIndex - VIDEO_FRAME_BUFFER),
+            currentFrameIndex + VIDEO_FRAME_BUFFER
+          )
+        : // If no frame is matched, still render an invisible placeholder so that the container has size
+          src.slice(0, 1);
 
     return {currentFrame, buffer};
   }
@@ -83,25 +93,32 @@ export default class ImageSequence extends PureComponent {
   };
 
   _renderFrame = (frame, i) => {
+    if (!frame) {
+      return null;
+    }
+
+    const {width, height} = this.props;
     const isVisible = frame === this.state.currentFrame;
 
-    return (
-      <img
-        key={frame.timestamp}
-        src={frame.imageUrl}
-        style={isVisible ? {filter: this._getVideoFilterCSS()} : null}
-        className={isVisible ? 'visible' : ''}
-      />
-    );
+    const style = {width, height, left: 0, top: 0};
+
+    if (i > 0) {
+      style.position = 'absolute';
+    }
+
+    if (isVisible) {
+      style.filter = this._getVideoFilterCSS();
+    } else if (i === 0) {
+      // the first image is always displayed for the container to have the correct size
+      style.visibility = 'hidden';
+    } else {
+      style.display = 'none';
+    }
+
+    return <img key={frame.timestamp} src={frame.imageUrl} style={style} />;
   };
 
   render() {
-    const {width, height} = this.props;
-
-    return (
-      <div className="vehicle-video" style={{width, height}}>
-        {this.state.buffer.map(this._renderFrame)}
-      </div>
-    );
+    return <div style={CONTAINER_STYLE}>{this.state.buffer.map(this._renderFrame)}</div>;
   }
 }
