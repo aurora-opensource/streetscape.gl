ded713783d05cd445a97b73061c9ab256abfb41a
Timothy Wojtaszek <twojtasz@uber.com>
2018-10-09 03:05:39 -0700

    Update documentation with an Example Guide (#87)
    - Separate out the portions that relate to the KITTI example vs the general
      XVIZ development guide

--- website/src/config.js
+++ website/src/config.js
@@ -19,7 +19,7 @@
 // THE SOFTWARE.
 
 export const PROJECT_TYPE = 'github';
-
+export const PROJECT_ORG = 'uber';
 export const PROJECT_NAME = 'streetscape.gl';
 
 export const PROJECT_URL = 'https://github.com/uber/streetscape.gl';
@@ -31,18 +31,24 @@ export const HOME_HEADING = 'streetscape.gl';
 
 export const HOME_RIGHT = null;
 
-export const HOME_BULLETS = [{
-  text: 'Visualize autonomy and robotics data via the XVIZ protocol',
-  desc: 'Our turn-key application lets you start streaming and visualizing your autonomy data as soon as it is converted to XVIZ',
-  img: 'images/icon-high-precision.svg'
-}, {
-  text: 'Customizable 3D Display Stack',
-  desc: 'Add your own deck.gl layers to visualize your own 3D data layers mixed with the dynamic XVIZ scene',
-  img: 'images/icon-layers.svg'
-}, {
-  text: 'Extensible, React-based UI Toolkit',
-  desc: 'Use the default viewer application or customize to fit your needs.',
-  img: 'images/icon-react.svg'
-}];
+export const HOME_BULLETS = [
+  {
+    text: 'Visualize autonomy and robotics data via the XVIZ protocol',
+    desc:
+      'Our turn-key application lets you start streaming and visualizing your autonomy data as soon as it is converted to XVIZ',
+    img: 'images/icon-high-precision.svg'
+  },
+  {
+    text: 'Customizable 3D Display Stack',
+    desc:
+      'Add your own deck.gl layers to visualize your own 3D data layers mixed with the dynamic XVIZ scene',
+    img: 'images/icon-layers.svg'
+  },
+  {
+    text: 'Extensible, React-based UI Toolkit',
+    desc: 'Use the default viewer application or customize to fit your needs.',
+    img: 'images/icon-react.svg'
+  }
+];
 
 export const ADDITIONAL_LINKS = [];
--- website/src/mdRoutes.js
+++ website/src/mdRoutes.js
@@ -18,65 +18,99 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-export default [{
-  name: 'Documentation',
-  path: '/documentation',
-  data: [{
-    name: 'Overview',
-    children: [{
-      name: 'Introduction',
-      markdown: require('../../docs/README.md')
-    }, {
-      name: 'Overview',
-      markdown: require('../../docs/overview.md')
-    }, {
-      name: 'What\'s New',
-      markdown: require('../../docs/whats-new.md')
-    }, {
-      name: 'Roadmap',
-      markdown: require('../../docs/roadmap.md')
-    }, {
-      name: 'FAQ',
-      markdown: require('../../docs/FAQ.md')
-    }, {
-      name: 'Running Demos',
-      markdown: require('../../docs/running-demos.md')
-    }, {
-      name: 'Develop/Contribute',
-      markdown: require('../../docs/develop.md')
-    }]
-  }, {
-    name: 'Developer Guide',
-    children: [{
-      name: 'Overview',
-      markdown: require('../../docs/developer-guide/README.md')
-    }, {
-      name: 'Installing',
-      markdown: require('../../docs/developer-guide/installing.md')
-    }, {
-      name: 'Convert Data to XVIZ',
-      markdown: require('../../docs/developer-guide/convert-data-to-xviz.md')
-    }, {
-      name: 'Use Custom deck.gl Layers',
-      markdown: require('../../docs/developer-guide/use-custom-deck-layers.md')
-    }, {
-      name: 'Building Custom Applications',
-      markdown: require('../../docs/developer-guide/build-custom-applications.md')
-    }, {
-      name: 'Advanced',
-      markdown: require('../../docs/developer-guide/advanced.md')
-    }]
-  }, {
-    name: 'API Reference',
-    children: [{
-      name: 'LogViewer',
-      markdown: require('../../docs/api-reference/log-viewer.md')
-    }, {
-      name: 'XVIZLoaderInterface',
-      markdown: require('../../docs/api-reference/xviz-loader-interface.md')
-    }, {
-      name: 'XVIZStreamLoader',
-      markdown: require('../../docs/api-reference/xviz-stream-loader.md')
-    }]
-  }]
-}];
+export default [
+  {
+    name: 'Documentation',
+    path: '/documentation',
+    data: [
+      {
+        name: 'Overview',
+        children: [
+          {
+            name: 'Introduction',
+            markdown: require('../../docs/README.md')
+          },
+          {
+            name: 'Overview',
+            markdown: require('../../docs/overview.md')
+          },
+          {
+            name: "What's New",
+            markdown: require('../../docs/whats-new.md')
+          },
+          {
+            name: 'Roadmap',
+            markdown: require('../../docs/roadmap.md')
+          },
+          {
+            name: 'FAQ',
+            markdown: require('../../docs/FAQ.md')
+          },
+          {
+            name: 'Develop/Contribute',
+            markdown: require('../../docs/develop.md')
+          }
+        ]
+      },
+      {
+        name: 'Example Guide',
+        children: [
+          {
+            name: 'Overview',
+            markdown: require('../../docs/example-guide/README.md')
+          },
+          {
+            name: 'Running the Example',
+            markdown: require('../../docs/example-guide/running-the-example.md')
+          },
+          {
+            name: 'Convert Data to XVIZ',
+            markdown: require('../../docs/example-guide/convert-data-to-xviz.md')
+          }
+        ]
+      },
+      {
+        name: 'Developer Guide',
+        children: [
+          {
+            name: 'Overview',
+            markdown: require('../../docs/developer-guide/README.md')
+          },
+          {
+            name: 'Installing',
+            markdown: require('../../docs/developer-guide/installing.md')
+          },
+          {
+            name: 'Use Custom deck.gl Layers',
+            markdown: require('../../docs/developer-guide/use-custom-deck-layers.md')
+          },
+          {
+            name: 'Building Custom Applications',
+            markdown: require('../../docs/developer-guide/build-custom-applications.md')
+          },
+          {
+            name: 'Advanced',
+            markdown: require('../../docs/developer-guide/advanced.md')
+          }
+        ]
+      },
+      {
+        name: 'API Reference',
+        children: [
+          {
+            name: 'LogViewer',
+            markdown: require('../../docs/api-reference/log-viewer.md')
+          },
+          {
+            name: 'XVIZLoaderInterface',
+            markdown: require('../../docs/api-reference/xviz-loader-interface.md')
+          },
+          {
+            name: 'XVIZStreamLoader',
+            markdown: require('../../docs/api-reference/xviz-stream-loader.md')
+          }
+        ]
+      }
+    ]
+  }
+];
