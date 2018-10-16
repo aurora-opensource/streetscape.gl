c6e2432a40d44b437ef2a2fd9bbfa38b9ec8f495
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2018-10-16 15:26:09 -0700

    Add website demo scaffold (#95)

---
+++ examples/clients/website-demo/src/app.js
@@ -0,0 +1,99 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+/* global document */
+/* eslint-disable no-console */
+import 'xviz-config';
+
+import React, {PureComponent} from 'react';
+import {render} from 'react-dom';
+
+import {
+  XVIZFileLoader,
+  LogViewer,
+  PlaybackControl,
+  // XvizPanel,
+  // VideoPanel,
+  VIEW_MODES
+} from 'streetscape.gl';
+// import {Form} from 'monochrome-ui';
+
+import {MAPBOX_TOKEN, MAP_STYLE, XVIZ_STYLE, CAR} from './constants';
+
+import './main.scss';
+
+class Example extends PureComponent {
+  state = {
+    log: new XVIZFileLoader({
+      logGuid: 'mock',
+      serverConfig: {
+        numberOfFrames: 155,
+        getFilePath: index => `/kitti/2011_09_26/2011_09_26_drive_0005_sync/${index + 1}-frame.glb`,
+        worker: require.resolve('./stream-data-worker'),
+        maxConcurrency: 4
+      }
+    }).on('error', console.error), // eslint-disable-line
+
+    settings: {
+      viewMode: 'PERSPECTIVE'
+    }
+  };
+
+  componentDidMount() {
+    this.state.log.connect();
+  }
+
+  _onSettingsChange = changedSettings => {
+    this.setState({
+      settings: {...this.state.settings, ...changedSettings}
+    });
+  };
+
+  render() {
+    const {log, settings} = this.state;
+
+    return (
+      <div id="container">
+        <div id="map-view">
+          <LogViewer
+            log={log}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+            mapStyle={MAP_STYLE}
+            car={CAR}
+            xvizStyles={XVIZ_STYLE}
+            viewMode={VIEW_MODES[settings.viewMode]}
+          />
+        </div>
+        <div id="timeline">
+          <PlaybackControl
+            width="100%"
+            log={log}
+            formatTimestamp={x => new Date(x).toUTCString()}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+render(<Example />, root);
---
+++ examples/clients/website-demo/src/constants.js
@@ -0,0 +1,43 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+export const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+
+export const MAP_STYLE = 'mapbox://styles/mapbox/dark-v9';
+
+// OBJ model width 2073mm, length 4946mm
+// Volkswagen Passat: width 1820mm, length 4780mm
+export const CAR = {
+  mesh: 'assets/car.obj',
+  origin: [1.08, -0.32, 0],
+  scale: 0.0009
+};
+
+export const SETTINGS = {
+  viewMode: {
+    type: 'select',
+    title: 'View Mode',
+    data: {TOP_DOWN: 'Top Down', PERSPECTIVE: 'Perspective', DRIVER: 'Driver'}
+  }
+};
+
+export const XVIZ_STYLE = {
+  '/tracklets/objects': [{class: 'selected', fillColor: '#ff8000aa'}]
+};
