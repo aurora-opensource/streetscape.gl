848aebcb17cadff955d1c08ea96a37112e331a0c
Xiaoji Chen <xiaoji@uber.com>
2018-11-09 15:19:41 -0800

    Use new XVIZ settings API

--- modules/core/src/loaders/xviz-loader-interface.js
+++ modules/core/src/loaders/xviz-loader-interface.js
@@ -1,4 +1,4 @@
-import {getXVIZSettings, getXVIZConfig} from '@xviz/parser';
+import {setXVIZSettings, getXVIZSettings, getXVIZConfig} from '@xviz/parser';
 import {clamp} from 'math.gl';
 
 import {getTimeSeries} from '../utils/metrics-helper';
@@ -16,6 +16,10 @@ export default class XVIZLoaderInterface {
     this._version = 0;
     this._updateTimer = null;
     this.timestamp = null;
+
+    setXVIZSettings({
+      currentMajorVersion: 2
+    });
   }
 
   /* Event types:
