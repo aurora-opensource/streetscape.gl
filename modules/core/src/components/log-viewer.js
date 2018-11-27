0277efb1d3bed9fbef4d18284d83bb1b8086f552
Tim Wojtaszek <twojtasz@uber.com>
2018-11-27 13:21:27 -0800

    Combine processing primitives from XVIZ properties features and lookAhead
    - they need to be together to be properly sorted by z
    - reduce the duplication of the XVIZ layer construction

--- modules/core/src/components/log-viewer.js
+++ modules/core/src/components/log-viewer.js
@@ -206,6 +206,9 @@ class Core3DViewer extends PureComponent {
 
     const objectStates = this.props.objectStates || this.state.objectStates;
     const streamFilter = normalizeStreamFilter(this.props.streamFilter);
+    const featuresAndFutures = Object.keys(streams)
+      .concat(Object.keys(lookAheads))
+      .filter(streamName => streamFilter(streamName) && streamSettings[streamName]);
 
     return [
       carMesh &&
@@ -228,10 +231,11 @@ class Core3DViewer extends PureComponent {
             getYaw: heading
           }
         }),
-      Object.keys(streams)
-        .filter(streamName => streamFilter(streamName) && streamSettings[streamName])
+      featuresAndFutures
         .map(streamName => {
-          const stream = streams[streamName];
+          // Check lookAheads first because it will contain the selected futures
+          // while streams would contain the full futures array
+          const stream = lookAheads[streamName] || streams[streamName];
           const streamMetadata = metadata.streams[streamName];
           const coordinateProps = resolveCoordinateTransform(
             frame,
@@ -239,7 +243,9 @@ class Core3DViewer extends PureComponent {
             getTransformMatrix
           );
 
-          if (stream.features && stream.features.length) {
+          // Support both features and lookAheads, respectively
+          const primitives = stream.features || stream;
+          if (primitives && primitives.length) {
             return new XVIZLayer({
               id: `xviz-${streamName}`,
               ...coordinateProps,
@@ -247,7 +253,7 @@ class Core3DViewer extends PureComponent {
               pickable: true,
               lightSettings: LIGHT_SETTINGS,
 
-              data: stream.features,
+              data: primitives,
               style: styleParser.getStylesheet(streamName),
               objectStates,
 
@@ -280,44 +286,6 @@ class Core3DViewer extends PureComponent {
         })
         .filter(Boolean)
         .sort((layer1, layer2) => layer1.props.zIndex - layer2.props.zIndex),
-      Object.keys(lookAheads)
-        .filter(streamName => streamFilter(streamName) && streamSettings[streamName])
-        .map(streamName => {
-          // TODO(twojtasz): XVIZ parsing places the selected futureInstances into the 'lookAhead' field
-          //                 See if this can be treated the same as features by iterating over 'streams'
-          const stream = lookAheads[streamName];
-          const streamMetadata = metadata.streams[streamName];
-          const coordinateProps = resolveCoordinateTransform(
-            frame,
-            streamMetadata,
-            getTransformMatrix
-          );
-
-          if (stream.length) {
-            return new XVIZLayer({
-              id: `xviz-${streamName}`,
-              ...coordinateProps,
-
-              pickable: true,
-              lightSettings: LIGHT_SETTINGS,
-
-              data: stream,
-              style: styleParser.getStylesheet(streamName),
-              objectStates,
-
-              // Hack: draw extruded polygons last to defeat depth test when rendering translucent objects
-              // This is not used by deck.gl, only used in this function to sort the layers
-              zIndex: streamMetadata.type === 'polygon' ? 2 : 0,
-
-              // Selection props (app defined, not used by deck.gl)
-              streamName
-            });
-          }
-          return null;
-        })
-        .filter(Boolean)
-        .sort((layer1, layer2) => layer1.props.zIndex - layer2.props.zIndex),
-
       customLayers.map(layer => {
         // Clone layer props
         const props = {...layer.props};
