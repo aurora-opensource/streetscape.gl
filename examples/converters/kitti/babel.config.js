17c3d8c4c9bf53a89eb598c285a09ff96625c5a6
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2018-12-06 14:57:52 -0800

    Move converter and server examples to xviz (#172)

--- examples/converters/kitti/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = function babelConfig(api) {
-  api.cache(() => process.env.NODE_ENV === 'production'); // eslint-disable-line
-
-  return {
-    presets: ['@babel/preset-env'],
-    plugins: [['@babel/plugin-proposal-class-properties', {loose: false}]]
-  };
-};
