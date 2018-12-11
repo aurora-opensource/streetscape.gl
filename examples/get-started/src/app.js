1b759929fdbc0175aaf3fd9aa147ca9c147e6fa9
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2018-12-11 15:26:37 -0800

    Update get-started docs (#181)

---
+++ examples/get-started/src/app.js
@@ -0,0 +1,83 @@
+/* global document, console */
+/* eslint-disable no-console, no-unused-vars */
+import React, {PureComponent} from 'react';
+import {render} from 'react-dom';
+
+import {setXVIZConfig, setXVIZSettings} from '@xviz/parser';
+import {LogViewer, PlaybackControl, XVIZPanel, VIEW_MODE} from 'streetscape.gl';
+import {Form} from '@streetscape.gl/monochrome';
+
+import {
+  XVIZ_CONFIG,
+  XVIZ_SETTINGS,
+  APP_SETTINGS,
+  MAPBOX_TOKEN,
+  MAP_STYLE,
+  XVIZ_STYLE,
+  CAR,
+  EXAMPLE_LOG_FROM_STREAM,
+  EXAMPLE_LOG_FROM_FILE
+} from './constants';
+
+setXVIZConfig(XVIZ_CONFIG);
+setXVIZSettings(XVIZ_SETTINGS);
+
+class Example extends PureComponent {
+  state = {
+    log: EXAMPLE_LOG_FROM_FILE.on('error', console.error),
+    settings: {
+      viewMode: 'PERSPECTIVE'
+    }
+  };
+
+  componentDidMount() {
+    this.state.log.connect();
+  }
+
+  _onSettingsChange = changedSettings => {
+    this.setState({
+      settings: {...this.state.settings, ...changedSettings}
+    });
+  };
+
+  render() {
+    const {log, settings} = this.state;
+
+    return (
+      <div id="container">
+        <div id="control-panel">
+          <XVIZPanel log={log} name="Camera" />
+          <hr />
+          <XVIZPanel log={log} name="Metrics" />
+          <hr />
+          <Form
+            data={APP_SETTINGS}
+            values={this.state.settings}
+            onChange={this._onSettingsChange}
+          />
+        </div>
+        <div id="log-panel">
+          <div id="map-view">
+            <LogViewer
+              log={log}
+              mapboxApiAccessToken={MAPBOX_TOKEN}
+              mapStyle={MAP_STYLE}
+              car={CAR}
+              xvizStyles={XVIZ_STYLE}
+              viewMode={VIEW_MODE[settings.viewMode]}
+            />
+          </div>
+          <div id="timeline">
+            <PlaybackControl
+              width="100%"
+              log={log}
+              formatTimestamp={x => new Date(x).toUTCString()}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<Example />, document.getElementById('app'));
---
+++ examples/get-started/src/constants.js
@@ -0,0 +1,54 @@
+import {XVIZStreamLoader, XVIZFileLoader} from 'streetscape.gl';
+
+/* eslint-disable camelcase */
+export const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+
+export const MAP_STYLE = 'mapbox://styles/mapbox/light-v9';
+
+export const XVIZ_CONFIG = {
+  PRIMARY_POSE_STREAM: '/vehicle_pose',
+  OBJECT_STREAM: '/tracklets/objects'
+};
+
+export const XVIZ_SETTINGS = {
+  PLAYBACK_FRAME_RATE: 10
+};
+
+export const EXAMPLE_LOG_FROM_STREAM = new XVIZStreamLoader({
+  logGuid: 'mock',
+  // bufferLength: 15000,
+  serverConfig: {
+    defaultLogLength: 30000,
+    serverUrl: 'ws://localhost:8081'
+  },
+  worker: true,
+  maxConcurrency: 4
+});
+
+export const EXAMPLE_LOG_FROM_FILE = new XVIZFileLoader({
+  timingsFilePath: 'https://uber.github.io/xviz-data/kitti/2011_09_26_drive_0005_sync/0-frame.json',
+  getFilePath: index =>
+    `https://uber.github.io/xviz-data/kitti/2011_09_26_drive_0005_sync/${index + 1}-frame.glb`,
+  worker: true,
+  maxConcurrency: 4
+});
+
+// OBJ model width 2073mm, length 4946mm
+// Volkswagen Passat: width 1820mm, length 4780mm
+export const CAR = {
+  mesh: 'assets/car.obj',
+  origin: [1.08, -0.32, 0],
+  scale: 0.0009
+};
+
+export const APP_SETTINGS = {
+  viewMode: {
+    type: 'select',
+    title: 'View Mode',
+    data: {TOP_DOWN: 'Top Down', PERSPECTIVE: 'Perspective', DRIVER: 'Driver'}
+  }
+};
+
+export const XVIZ_STYLE = {
+  '/tracklets/objects': [{name: 'selected', style: {fill_color: '#ff8000aa'}}]
+};
