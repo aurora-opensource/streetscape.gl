979bb88007a46bdba5809a5b1198d32c732dde0d
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2018-12-10 21:48:52 -0800

    Apply streetscape icons to website demo (#179)

--- examples/website-demo/src/metadata-panel.js
+++ examples/website-demo/src/metadata-panel.js
@@ -1,7 +1,28 @@
 import React, {PureComponent} from 'react';
 import {connectToLog} from 'streetscape.gl';
+import {Dropdown} from '@streetscape.gl/monochrome';
+import {LOGS} from './constants';
 
 class MetadataPanel extends PureComponent {
+  _renderLogSelector() {
+    const {selectedLog} = this.props;
+
+    const logs = LOGS.reduce((resMap, log) => {
+      resMap[log.name] = log.name;
+      return resMap;
+    }, {});
+
+    return (
+      <div>
+        <Dropdown
+          value={selectedLog.name}
+          data={logs}
+          onChange={value => this.props.onLogChange(LOGS.find(log => log.name === value))}
+        />
+      </div>
+    );
+  }
+
   render() {
     const {metadata} = this.props;
 
@@ -11,12 +32,17 @@ class MetadataPanel extends PureComponent {
 
     return (
       <div>
-        <h5>Version</h5>
-        <p>{metadata.version}</p>
-        <h5>Log Start Time</h5>
-        <p>{new Date(metadata.start_time).toJSON()}</p>
-        <h5>Log End Time</h5>
-        <p>{new Date(metadata.end_time).toJSON()}</p>
+        <h4>Select a Log</h4>
+        {this._renderLogSelector()}
+
+        <h4>XVIZ Version</h4>
+        <div>{metadata.version}</div>
+
+        <h4>Log Start Time</h4>
+        <div>{new Date(metadata.start_time).toJSON()}</div>
+
+        <h4>Log End Time</h4>
+        <div>{new Date(metadata.end_time).toJSON()}</div>
       </div>
     );
   }
