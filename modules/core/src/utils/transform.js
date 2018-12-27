e83929965d20eb7a8dd2e11e99ff53caa99d32b5
Xiaoji Chen <xiaoji@uber.com>
2018-12-27 15:14:54 -0800

    add comments

--- modules/core/src/utils/transform.js
+++ modules/core/src/utils/transform.js
@@ -4,6 +4,7 @@ import {addMetersToLngLat} from 'viewport-mercator-project';
 
 import {COORDINATE} from '../constants';
 
+// keep in sync with core-3d-viewer.js
 const DEFAULT_ORIGIN = [0, 0, 0];
 
 export function resolveCoordinateTransform(frame, streamMetadata = {}, getTransformMatrix) {
