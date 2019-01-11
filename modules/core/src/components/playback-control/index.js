d46c8b8415bae14b2eadfc541d5dd28c4bc34a91
Xiaoji Chen <xiaoji@uber.com>
2019-01-10 20:36:16 -0800

    minor component bug fixes

--- modules/core/src/components/playback-control/index.js
+++ modules/core/src/components/playback-control/index.js
@@ -172,7 +172,7 @@ class PlaybackControl extends PureComponent {
   render() {
     const {startTime, endTime, timestamp, lookAhead, bufferRange, ...otherProps} = this.props;
 
-    if (!Number.isFinite(timestamp)) {
+    if (!Number.isFinite(timestamp) || !Number.isFinite(startTime)) {
       return null;
     }
 
