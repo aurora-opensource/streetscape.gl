1a1d7992124b7c25592a8d6f452462672d4ac5be
Xiaoji Chen <xiaoji@uber.com>
2019-01-10 21:05:49 -0800

    move map layers into separate module

--- modules/layers/src/imagery-layer/imagery-layer.js
+++ modules/layers/src/imagery-layer/imagery-layer.js
@@ -19,13 +19,42 @@
 // THE SOFTWARE.
 
 import {Layer} from '@deck.gl/core';
-import {Model, Geometry} from 'luma.gl';
+import {Model, Geometry, loadTextures, Texture2D} from 'luma.gl';
 
 import IMAGERY_VERTEX_SHADER from './imagery-layer-vertex';
 import IMAGERY_FRAGMENT_SHADER from './imagery-layer-fragment';
 
-import {getTexture} from '../layer-utils';
 import GridGeometry from './grid-geometry';
+
+/*
+ * Load image data into luma.gl Texture2D objects
+ * @param {WebGLContext} gl
+ * @param {String|Texture2D|HTMLImageElement|Uint8ClampedArray} src - source of image data
+ *   can be url string, Texture2D object, HTMLImageElement or pixel array
+ * @returns {Promise} resolves to an object with name -> texture mapping
+ */
+function getTexture(gl, src, opts) {
+  if (typeof src === 'string') {
+    // Url, load the image
+    return loadTextures(gl, {urls: [src], ...opts})
+      .then(textures => textures[0])
+      .catch(error => {
+        throw new Error(`Could not load texture from ${src}: ${error}`);
+      });
+  }
+  return new Promise(resolve => resolve(getTextureFromData(gl, src, opts)));
+}
+
+/*
+ * Convert image data into texture
+ * @returns {Texture2D} texture
+ */
+function getTextureFromData(gl, data, opts) {
+  if (data instanceof Texture2D) {
+    return data;
+  }
+  return new Texture2D(gl, {data, ...opts});
+}
 
 /*
  * @class
