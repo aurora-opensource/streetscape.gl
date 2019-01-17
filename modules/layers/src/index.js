951f31baf3fdee4f5981712dd2a4404831d227b0
Kevin Sullivan <kevin.sullivan@axolotl.industries>
2019-01-17 14:48:11 -0500

    Fix imports of layers module (#225)

--- modules/layers/src/index.js
+++ modules/layers/src/index.js
@@ -18,6 +18,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-export {default as SignLayer} from './layers/sign-layer/sign-layer';
-export {default as TrafficLightLayer} from './layers/traffic-light-layer/traffic-light-layer';
-export {default as ImageryLayer} from './layers/imagery-layer/imagery-layer';
+export {default as SignLayer} from './sign-layer/sign-layer';
+export {default as TrafficLightLayer} from './traffic-light-layer/traffic-light-layer';
+export {default as ImageryLayer} from './imagery-layer/imagery-layer';
