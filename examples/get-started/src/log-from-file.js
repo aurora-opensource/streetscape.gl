4901c7316bf621e21862d27f1358c230699726e5
Xiaoji Chen <xiaoji@uber.com>
2019-02-19 09:10:04 -0800

    release prep

--- examples/get-started/src/log-from-file.js
+++ examples/get-started/src/log-from-file.js
@@ -21,9 +21,11 @@
 import {XVIZFileLoader} from 'streetscape.gl';
 
 export default new XVIZFileLoader({
-  timingsFilePath: 'https://uber.github.io/xviz-data/kitti/2011_09_26_drive_0005_sync/0-frame.json',
+  timingsFilePath:
+    'https://raw.githubusercontent.com/uber/xviz-data/master/kitti/2011_09_26_drive_0005_sync/0-frame.json',
   getFilePath: index =>
-    `https://uber.github.io/xviz-data/kitti/2011_09_26_drive_0005_sync/${index + 1}-frame.glb`,
-  worker: true,
+    `https://raw.githubusercontent.com/uber/xviz-data/master/kitti/2011_09_26_drive_0005_sync/${index +
+      1}-frame.glb`,
+  // worker: true,
   maxConcurrency: 4
 });
