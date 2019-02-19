8a6dd0e6976e6cad44bc4b0a1eaa5ad3738d5bca
Xiaoji Chen <xiaoji@uber.com>
2019-02-19 12:39:46 -0800

    re-enable worker on get-started example

--- examples/get-started/src/log-from-file.js
+++ examples/get-started/src/log-from-file.js
@@ -26,6 +26,6 @@ export default new XVIZFileLoader({
   getFilePath: index =>
     `https://raw.githubusercontent.com/uber/xviz-data/master/kitti/2011_09_26_drive_0005_sync/${index +
       1}-frame.glb`,
-  // worker: true,
+  worker: true,
   maxConcurrency: 4
 });
