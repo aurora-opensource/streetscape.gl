3b08f6dbf94bc7862fec7c5a46074c3fb350bf33
Tim Wojtaszek <twojtasz@uber.com>
2019-08-16 15:12:50 -0700

    Add geojson custom layer

--- examples/get-started/src/app.js
+++ examples/get-started/src/app.js
@@ -21,6 +21,7 @@
 /* global document, console */
 /* eslint-disable no-console, no-unused-vars, no-undef */
 import React, {PureComponent} from 'react';
+import {GeoJsonLayer} from '@deck.gl/layers';
 import {render} from 'react-dom';
 
 import {setXVIZConfig, getXVIZConfig} from '@xviz/parser';
@@ -42,6 +43,176 @@ setXVIZConfig(XVIZ_CONFIG);
 
 const TIMEFORMAT_SCALE = getXVIZConfig().TIMESTAMP_FORMAT === 'seconds' ? 1000 : 1;
 
+const geojson = 
+ {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            8.420505523681639,
+            49.01299163695282,
+            112
+          ],
+          [
+            8.424475193023682,
+            49.01282275074038,
+            112
+          ],
+          [
+            8.424324989318848,
+            49.01172497639439,
+            112
+          ],
+          [
+            8.420140743255615,
+            49.01218942234111,
+            112
+          ],
+          [
+            8.420076370239258,
+            49.01102126215874,
+            112
+          ],
+          [
+            8.424303531646729,
+            49.01083829482822,
+            112
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              8.425226211547852,
+              49.01121830314735,
+              112
+            ],
+            [
+              8.425140380859375,
+              49.01028938880215,
+              112
+            ],
+            [
+              8.427286148071289,
+              49.01030346339125,
+              112
+            ],
+            [
+              8.427286148071289,
+              49.01110570839222,
+              112
+            ],
+            [
+              8.426942825317383,
+              49.01195016284906,
+              112
+            ],
+            [
+              8.425590991973877,
+              49.01169682801593,
+              112
+            ],
+            [
+              8.425226211547852,
+              49.01121830314735,
+              112
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              8.425569534301758,
+              49.012879046208184,
+              112
+            ],
+            [
+              8.425569534301758,
+              49.012879046208184,
+              112
+            ],
+            [
+              8.425569534301758,
+              49.012879046208184,
+              112
+            ],
+            [
+              8.425569534301758,
+              49.012879046208184,
+              112
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              8.42350959777832,
+              49.01275238131608,
+              112
+            ],
+            [
+              8.425376415252686,
+              49.01275238131608,
+              112
+            ],
+            [
+              8.425376415252686,
+              49.01396272155595,
+              112
+            ],
+            [
+              8.42350959777832,
+              49.01396272155595,
+              112
+            ],
+            [
+              8.42350959777832,
+              49.01275238131608,
+              112
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.423101902008057,
+          49.011429417626935,
+          112
+        ]
+      }
+    }
+  ]
+};
 // __IS_STREAMING__ and __IS_LIVE__ are defined in webpack.config.js
 const exampleLog = require(__IS_STREAMING__
   ? './log-from-stream'
@@ -70,6 +241,20 @@ class Example extends PureComponent {
 
   render() {
     const {log, settings} = this.state;
+    const frame = log.getCurrentFrame();
+    const customLayers = [
+      new GeoJsonLayer({
+        id: 'map-geojson-layer',
+        // The layer is only re-generated if this URL changes
+        data: geojson,
+        stroked: true,
+        filled: true,
+        extruded: false,
+        getFillColor: f => f && f.properties && f.properties.fill || [255, 0, 0],
+        getLineColor: f => f && f.properties && f.properties.stroke || [0, 255, 0],
+        getLineWidth: 0.5
+      })
+    ];
 
     return (
       <div id="container">
@@ -95,6 +280,7 @@ class Example extends PureComponent {
               xvizStyles={XVIZ_STYLE}
               showTooltip={settings.showTooltip}
               viewMode={VIEW_MODE[settings.viewMode]}
+              customLayers={customLayers}
             />
             <div id="hud">
               <TurnSignalWidget log={log} streamName="/vehicle/turn_signal" />
