43ade3d8392b5f938698d5fd49c5e7f93f402827
Xiaoji Chen <xiaoji@uber.com>
2019-10-02 16:34:24 -0700

    poc: measurement tool

---
+++ test/apps/measure/src/app.js
@@ -0,0 +1,65 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+/* global document */
+/* eslint-disable no-unused-vars, no-undef */
+import React, {PureComponent} from 'react';
+import {render} from 'react-dom';
+
+import {setXVIZConfig} from '@xviz/parser';
+import {LogViewer, PlaybackControl, VIEW_MODE} from 'streetscape.gl';
+
+import {XVIZ_CONFIG, MAPBOX_TOKEN, MAP_STYLE, CAR} from './constants';
+import log from './log-from-file';
+import TapeMeasureLayer from './tape-measure-layer';
+
+setXVIZConfig(XVIZ_CONFIG);
+
+class Example extends PureComponent {
+  componentDidMount() {
+    log.connect();
+  }
+
+  render() {
+    const customLayers = [new TapeMeasureLayer({id: 'tape-measure', zIndex: 99})];
+
+    return (
+      <div id="container">
+        <LogViewer
+          log={log}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          mapStyle={MAP_STYLE}
+          car={CAR}
+          customLayers={customLayers}
+          viewMode={VIEW_MODE.PERSPECTIVE}
+        />
+        <div id="timeline">
+          <PlaybackControl
+            width="100%"
+            log={log}
+            formatTimestamp={x => new Date(x).toUTCString()}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<Example />, document.getElementById('app'));
